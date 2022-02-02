25adc2a1073c45ac9a3c2a8219804b10fa1997a8
Marko <mbudisic@clarkson.edu>
2022-02-02 10:03:27 -0500

    Started adding the stream function

--- src/VortexDynamics2D.jl
+++ src/VortexDynamics2D.jl
@@ -1,5 +1,24 @@
 module VortexDynamics2D
 
-# Write your package code here.
+"""
+    streamfunction(x; p, Γ)
+
+Evaluate the stream function of a collection of 2d vortices with positions
+in vector `p` and circulations in vector `Γ` using the Biot--Savart law.
+
+
+The length of vector `Γ` determines the number of vortices `N`.
+The length of vector `p` should be exactly `2N` with coordinates of vortices 
+interlaced, i.e., `x,y,x,y,...` for 
+"""
+function streamfunction(x, p, Γ)
+
+    
+    2x + 3y
+    
+end
+
+export streamfunction
+
 
 end
