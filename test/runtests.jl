a79a3b6acefda8773fa6791d1c812eaf8c72505b
Marko <mbudisic@clarkson.edu>
2022-03-22 11:50:59 -0400

    Demo in runtests.jl works OK

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,16 +1,13 @@
-using VortexDynamics2D
+N = 2;
 
-
-N = 4;
-
-c1 = [-8.  -2.] .- rand(N,2);
-c2 = [ 8.  -2.] .- rand(N,2);
-c3 = [-8.   2.] .- rand(N,2);
-c4 = [ 8.   2.] .- rand(N,2);
+c1 = [-3.  -2.] .- rand(N,2);
+c2 = [ 3.  -2.] .- rand(N,2);
+c3 = [-3.   2.] .- rand(N,2);
+c4 = [ 3.   2.] .- rand(N,2);
 
 c = [c1;c2;c3;c4];
 
-Γ = -10.0 * sign.(c[:,2]).*sign.(c[:,1])/N + randn(4*N,1)/N; 
+Γ = 10.0 * sign.(c[:,2]).*sign.(c[:,1])/N + randn(4*N,1)/N;
 
 c = c[:]
 Γ = Γ[:]
@@ -22,24 +19,21 @@ vf(u,p,t) = VortexDynamics2D.biotsavart(u,u,Γ)
 using DifferentialEquations
 
 u0 = c[:]
-tspan = (0, 20.0)
+tspan = (0, 1000.0)
 
 prob = ODEProblem(
     (u,p,t) -> VortexDynamics2D.biotsavart(u,u,Γ),
-    u0, 
+    u0,
     tspan)
 
-sol = solve(prob, saveat=0.1)
+sol = solve(prob,AutoTsit5(Rosenbrock23()),reltol=1e-3,abstol=1e-3,saveat=0.1)
 
-
-using Plots
-
-@userplot Vortices
-@recipe function f(cp::Vortices)
-    sol, t = cp.args
-    
-
+M = length(Γ)
+pp = plot( sol[1,:], sol[1+M,:])
+for k=2:M
+    plot!(pp, sol[k,:], sol[k+M,:])
 end
+@show pp
 
 # using Test
 # @testset "VortexDynamics2D.jl" begin
