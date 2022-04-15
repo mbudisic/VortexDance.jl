616f5730dd55370352d7a95f7adee41aa7c2ef71
Marko <mbudisic@clarkson.edu>
2022-04-15 15:09:22 -0400

    Small updates

--- src/VortexDynamics2D.jl
+++ src/VortexDynamics2D.jl
@@ -19,7 +19,7 @@ uses a different aggregation function across vortices. e.g. `aggregate=maximum`
 will use the strongest contribution, not the sum of contributions. This is nonphysical 
 but can be of interest.
 """
-function biotsavart(pe,pv, Γ; aggregate=:sum, core=1.0e-6)
+function biotsavart(pe, pv, Γ; aggregate=:sum, core=1.0e-6)
 
     #TODO Convert to non-allocating
     #TODO Add multiple-dispatch depending on the shape of inputs pv pe
@@ -28,25 +28,25 @@ function biotsavart(pe,pv, Γ; aggregate=:sum, core=1.0e-6)
     Ne = length(pe) ÷ 2
 
     pvx = @views pv[1:Nv]
-    pvy = @views pv[(Nv +1):end]
+    pvy = @views pv[(Nv+1):end]
 
     pex = @views pe[1:Ne]
-    pey = @views pe[(Ne + 1):end]
+    pey = @views pe[(Ne+1):end]
 
     # ensure that number of vertices is the same as number of circulations
     @assert size(pvx) == size(Γ)
 
-    D = zeros( Ne,Nv, 2 )
-    Lsq = zeros( Ne,Nv , 1 )
+    D = zeros(Ne, Nv, 2)
+    Lsq = zeros(Ne, Nv, 1)
 
-    vectors_normal_to_pairs!(D, Lsq, [pex pey],[pvx pvy],core )
+    vectors_normal_to_pairs!(D, Lsq, [pex pey], [pvx pvy], core)
     # compute the Biot--Savart
-    perp_gradΨ  =  - (transpose(Γ)/2/π) .* D ./ Lsq ; 
+    perp_gradΨ = -(transpose(Γ) / 2 / π) .* D ./ Lsq
 
     # add all vortex influences  size(V) = #evaluation points x 1 x 2
-    V = sum( perp_gradΨ, dims=2 );
+    V = sum(perp_gradΨ, dims=2)
     V = V[:]
-    
+
 
 end
 
@@ -61,15 +61,15 @@ L is N1 x N2 x 1
 If optional argument is passed, distances shorter than core_dist are 
 set to core_dist, and the associated perp vectors are set to zero vectors
 """
-function vectors_normal_to_pairs!( D, Lsq, p1, p2, core_dist = 0)
-    @views D[:,:,1] .= p1[:,2] .- transpose( p2[:,2] )
-    @views D[:,:,2] .=  -( p1[:,1] .- transpose( p2[:,1] ) )
+function vectors_normal_to_pairs!(D, Lsq, p1, p2, core_dist=0)
+    @views D[:, :, 1] .= p1[:, 2] .- transpose(p2[:, 2])
+    @views D[:, :, 2] .= -(p1[:, 1] .- transpose(p2[:, 1]))
 
-    sum!( Lsq, D.^2 ) # compute euclidean norm for each D(i,j,:)
+    sum!(Lsq, D .^ 2) # compute euclidean norm for each D(i,j,:)
 
     sel = @. Lsq <= core_dist^2
-    @. D[ sel, : ] = 0
-    @. Lsq[ sel ] = core_dist^2 
+    @. D[sel, :] = 0
+    @. Lsq[sel] = core_dist^2
 
 end
 
