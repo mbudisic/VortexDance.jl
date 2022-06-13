782885829c4e25bba76ab28a9f0cea2c148961a6
Marko <mbudisic@clarkson.edu>
2022-06-13 16:33:59 -0400

    Back to AbstractVector - matrix is a bad idea

--- src/VortexDance.jl
+++ src/VortexDance.jl
@@ -62,7 +62,7 @@ end
 
 
 """
-function biotsavart( pe::AbstractMatrix{T}, pv::Vector{T}, Γ::Vector; core=1e-12) where {T <: Vec2D}
+function biotsavart( pe::AbstractVector{T}, pv::Vector{T}, Γ::Vector; core=1e-12) where {T <: Vec2D}
     
     # ensure that number of vertices is the same as number of circulations
     @assert length(pv) == length(Γ)
