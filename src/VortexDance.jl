c559dfb553e8d3b036d56c4ecc42eb087a28aeea
Marko <mbudisic@clarkson.edu>
2022-06-14 23:03:32 -0400

    Redesign biotsavart interface Allow for return of only velocity field or stream function, but not both (introduces confusion between scalar/tuple return values)

--- src/VortexDance.jl
+++ src/VortexDance.jl
@@ -7,18 +7,24 @@ using StaticArrays
 export vortexdance
 export biotsavart
 export Vec2D
+export vortex_ODE
 
-const Vec2D{T} = StaticArrays.SVector{2,T}
+const Vec2D = StaticArrays.SVector{2}
+
+
+function vortex_ODE(u,p,t)
+    out = biotsavart(u, u, p; fieldtype=:velocity)        
+    return convert(typeof(u),out)
+end
 
 """
 This version of the function creates a return function that can be interpolated
 """
 function vortexdance( p_initial, Γ, tspan::Tuple{Real, Real} )
     
-    velocityfield(u, p, t) = biotsavart(u, u, p; fieldtype=:velocity)
-        
+    
     diffeq = ODEProblem(
-    velocityfield,
+    vortex_ODE,
     p_initial,
     tspan,
     Γ)
@@ -36,13 +42,13 @@ end
 """
 This version of the function creates an interpolated array
 """
-function vortexdance( p_initial, Γ, T::AbstractVector )
+function vortexdance( p_initial, Γ, Trange::AbstractVector )
 
     # evaluate solution for the endpoints of the range
-    sol, diffeq = vortexdance(p_initial, Γ, (first(T), last(T)) )
+    sol, diffeq = vortexdance(p_initial, Γ, (first(Trange), last(Trange)) )
 
     # interpolate solution
-    sol_traces = sol(T)
+    sol_traces = sol(Trange)
 
     # create a matrix of the solution
     sol_matrix = mapreduce( permutedims, vcat, sol_traces.u )
@@ -54,9 +60,8 @@ end
 
 function biotsavart( pe::Vec2D, pv::Vector{T}, Γ::Vector; args...) where {T <: Vec2D}
     
-    retvalues =  biotsavart( [pe], pv, Γ; args...)
-    return first.( (retvalues...) )
-
+    first(biotsavart( [pe,], pv, Γ; args...))
+    
 end
 
 """
@@ -65,9 +70,12 @@ end
 """
 function biotsavart( pe::AbstractVector{T}, pv::Vector{T}, Γ::Vector; core=1e-12, fieldtype::Union{Symbol,Vector{Symbol}} =[:velocity, :stream]
     ) where {T <: Vec2D}
-    
+
     # ensure that number of vertices is the same as number of circulations
     @assert length(pv) == length(Γ)
+
+    # ensure fieldtype is either velocity function or stream function
+    @assert fieldtype in [:velocity, :stream]
 
     # ensure that the core is a non-negative number
     @assert core >= 0
@@ -83,33 +91,34 @@ function biotsavart( pe::AbstractVector{T}, pv::Vector{T}, Γ::Vector; core=1e-1
     distances = norm.(normals)
     
 
-    retfields = ()
 
-    fieldtype = vcat([fieldtype,]...) # ensure both scalar and 
-                                      # vector of symbols are accepted
-    @show fieldtype
-    if :velocity in fieldtype
+    if fieldtype == :velocity
     # individual contribution to velocity fieldtype and stream function
     vfs = -(1/2/π) * (Γ' .* normals ) ./ (distances .^ 2 .+ core .^2 )
-    vf = vec(  sum(vfs, dims=2) )
 
-    retfields = (retfields..., vf)
-    end
+    vf = similar(pe, VortexDance.Vec2D)
+    vf .= sum(vfs, dims=2)
 
-    if :stream in fieldtype
+    @assert size(vf) == size(pe)
+
+    return vf
+    
+    elseif fieldtype==:stream
     streams = (-1/2/π) * Γ' .* log.(distances .+ core)
     
     # sum all contributions
-    stream = vec( sum(streams, dims=2) )
+    stream = similar(pe, typeof(streams[1]))
+    stream .= sum(streams, dims=2)
 
-    retfields = (retfields..., stream)
+    @assert size(stream) == size(pe)
+    return stream
+    
     end
 
     # note to future self: 
     #   once we're trying to implement "max" or "min" 
     #   instead "sum" use findmax/findmin
     
-    return retfields
     
 end
 
